7cfed60451017cf3a5bf001297704aa4f1c68123
Zach Yanez <zachyanez@gmail.com>
2021-01-16 16:54:11 -0600

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+export default function Header() {
+  return (
+    <div>
+      <Navbar bg="l" variant="light" fixed="top">
+        <Navbar.Brand>HeyFrom6</Navbar.Brand>
+        <Nav className="mr-auto">
+          <DropdownButton id="dropdown-item-button" title="Presets">
+            <Dropdown.Item as="button">Hey, I'm Zach</Dropdown.Item>
+            <Dropdown.Item as="button">512-740-3455</Dropdown.Item>
+            <Dropdown.Item as="button">What's your name?</Dropdown.Item>
+            <Dropdown.Item as="button">Something else</Dropdown.Item>
+          </DropdownButton>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Message" className="mr-sm-2" />
+          <Button variant="outline-primary">Say It</Button>
+        </Form>
+      </Navbar>
+    </div>
+  );
+}
