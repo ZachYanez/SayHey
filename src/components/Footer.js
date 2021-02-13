3ca0875fc456b9435b56eedd65632fed25ffe802
Zach Yanez <zachyanez@gmail.com>
2021-02-12 21:21:06 -0600

    fixing delete alert

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default function Footer() {
           <a className="footerLink" rel="noreferrer" target="_blank" href="https://zachyanez.com">
             By Zach Yanez
           </a>
-          <a className="Install" target="_blank" href="https://docs.google.com/document/d/1cJESFTKpnsXO5vtK2foLeTPzledAYLUTQNWOR5kdMV8/edit">[Instructions]</a>
+          <a className="Install" target="_blank" href="https://docs.google.com/document/d/1cJESFTKpnsXO5vtK2foLeTPzledAYLUTQNWOR5kdMV8/edit?usp=sharing">[Instructions]</a>
         </Navbar.Brand>
       </Navbar>
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -62,7 +62,7 @@ export default function Main() {
 
   function deletePreset(id) {
     deleteRecord(id).then(() => {
-      alert(`Deleted ${id}`);
+      alert(`Deleted`);
     });
 
     setPresets((prevPresets) => {
